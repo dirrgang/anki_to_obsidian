55183ded297ec4e06e7cd1ef41331dbfb2df8068
Dennis Irrgang <d@irrgang.dev>
2021-12-02 16:35:31 +0000

    First working prototype

--- anki_to_obsidian.py
+++ anki_to_obsidian.py
@@ -11,26 +11,72 @@ from zipfile import ZipFile
 import sqlite3
 import re
 import markdownify as md
+import os
+from pathvalidate import sanitize_filename
 
 
-def open_apkg():
+def open_apkg(file):
 
-    with ZipFile('decks.apkg', 'r') as zip_obj:
+    with ZipFile(file, 'r') as zip_obj:
         zip_obj.extractall('tmp')
 
     con = sqlite3.connect("tmp/collection.anki21")
     cur = con.cursor()
     cur.execute("SELECT tags, flds, sfld FROM notes")
+    return cur
 
-    mjregex_open = re.compile(re.escape('\('))
-    mjregex_close = re.compile(re.escape('\)'))
-    html_body = re.sub(mjregex_open, '$', re.sub(
-        mjregex_close, '$', cur.fetchone()[1]))
 
-    markdown_body = md.markdownify(html_body, heading_style="ATX")
-    print(markdown_body)
+def remove_cloze(text):
+    cloze_regex = re.compile(r'({{c\d+::.*?}})')
+    text_list = re.split(cloze_regex, text)
+
+    result = ""
+    for item in text_list:
+        if item[0:3] == r'{{c':
+            result += re.sub(r'{{c\d+::', '', item)[:-2]
+        else:
+            result += item
+    return result
+
+
+def modify_mathjax(text):
+
+    mjregex = re.compile(r'(\\\(.*?\\\))')
+    text_list = re.split(mjregex, text)
+
+    result = ""
+    for item in text_list:
+        if item[0:2] == r'\(':
+            result += '$'+item[2:-2].strip()+'$'
+        else:
+            result += item
+
+    return result
 
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    open_apkg()
+    cur = open_apkg('decks.apkg')
+
+    records = cur.fetchall()
+
+    for row in records:
+
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(
+            dirname, 'export/'+sanitize_filename(row[2]).strip()+'.txt')
+
+        f = open(filename, mode="w")
+
+        body = remove_cloze(row[1])
+        body = modify_mathjax(body)
+
+        body = md.markdownify(body, heading_style="ATX")
+
+        body = re.sub(r'', '\n', body)
+        f.write(body)
+        f.write('\n')
+
+        tags = row[0].split()
+
+        for tag in tags:
+            f.write('#'+tag+' ')
